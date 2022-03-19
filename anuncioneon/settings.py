3bf7a90c6942290e26986afa4538229d86e955a3
joselws <josel.wilhelms@gmail.com>
2022-03-19 09:36:11 -0400

    minor change

--- anuncioneon/settings.py
+++ anuncioneon/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'anuncioneon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
